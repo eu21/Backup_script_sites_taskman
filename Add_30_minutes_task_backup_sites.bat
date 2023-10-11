c7cdbfe4b21b03a78987dbfa885972257e900fad
Kuzniatsou Yauheni <yauheni_kuzniatsou@epam.com>
2023-10-11 17:28:42 +0300

    Add existing project files to Git

---
+++ Add_30_minutes_task_backup_sites.bat
@@ -0,0 +1,34 @@
+REM Add windows scheduler task that will run every 30 minutes
+REM Programm:
+REM "C:\Program Files\Git\git-bash.exe"
+REM Args:
+REM %currentDir%exec_all_run.repo
+
+
+
+@echo off
+:: Check if the script is running as administrator
+net session >nul 2>&1
+if %errorLevel% == 0 (
+    goto :runScript
+) else (
+    echo Requesting administrative privileges...
+    goto :getAdmin
+)
+
+:getAdmin
+:: Request administrative privileges
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs"
+exit /b
+
+
+
+:runScript
+:: Get the script directory
+set "currentDir=%~dp0"
+
+:: Create the scheduled task
+schtasks /create /tn "Backup_script_SITES" /tr "\"%currentDir%Backup_script_SITES.bat\"" /sc minute /mo 30 /ru System
+
